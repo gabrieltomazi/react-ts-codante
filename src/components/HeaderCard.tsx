54496d23ee7d54266700129ca159dd1b187a5f81
gabrieltomazi <tomazigabriel3@gmail.com>
2026-05-07 11:53:47 -0300

    feat: resolucao events e event handlers

---
+++ src/components/HeaderCard.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef } from "react";
+import { fetchRandomColor } from "../lib/services";
+import Button from "./Button";
+
+type HeaderCardProps = {
+  handleChangeQuoteClick: React.MouseEventHandler<HTMLButtonElement>;
+  setBgColor: React.Dispatch<React.SetStateAction<string>>;
+  bgColor: string;
+};
+
+export default function HeaderCard({
+  handleChangeQuoteClick,
+  bgColor,
+  setBgColor,
+}: HeaderCardProps) {
+  //
+  async function handleChangeColorClick() {
+    const newColor = await fetchRandomColor();
+    setBgColor(newColor.hex);
+  }
+
+  const quoteButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    quoteButtonRef?.current?.focus();
+  }, []);
+
+  // function handleChangeColorPick(
+  //   e: React.ChangeEventHandler<HTMLInputElement>,
+  // ) {
+  //   setBgColor(e.target.value);
+  // }
+
+  return (
+    <div className="w-full max-w-4xl px-10 py-6 mx-auto bg-white border-2 border-white rounded-lg bg-opacity-15 ">
+      <h1
+        className="text-3xl font-bold brightness-50 md:text-7xl font-display "
+        style={{ color: bgColor }}
+      >
+        Frases Motivacionais
+      </h1>
+
+      <Button ref={quoteButtonRef} onClick={handleChangeQuoteClick}>
+        Me dê uma dose de ânimo
+      </Button>
+
+      <Button onClick={handleChangeColorClick}>Trocar de cor</Button>
+
+      <label
+        htmlFor="color-changer"
+        className="inline-flex items-center justify-center px-4 py-2 mt-10 ml-2
+        font-bold text-white bg-transparent Dbg-gray-800 border-2 border-white
+        rounded bg-opacity-10 focus:outline-none focus:ring-2 focus: ring-white
+        focus:ring-opacity-50 cursor-pointer"
+      >
+        <span>Escolha uma cor</span>
+        <input
+          type="color"
+          name="color-changer"
+          id="color-changer"
+          onChange={(e) => setBgColor(e.target.value)}
+          className="w-0 h-0"
+        />
+      </label>
+    </div>
+  );
+}
